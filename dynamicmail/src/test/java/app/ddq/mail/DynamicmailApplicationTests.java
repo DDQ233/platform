db64d3c058995db44efe4cfcd6ff00399166c17a
DDQ <2841730354@qq.com>
2020-08-25 09:46:50 +0800

    定时邮件任务清单项目初始化

---
+++ dynamicmail/src/test/java/app/ddq/mail/DynamicmailApplicationTests.java
@@ -0,0 +1,13 @@
+package app.ddq.mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
